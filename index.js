2161bca5fef36659eac5bb99bfc5898732bdf331
Rahat301 <mdrahathossain924@gmail.com>
2023-06-05 19:16:27 +0600

    username and pass secure successfull with dotnev

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const ObjectId = require("mongodb").ObjectId;
 
 const app = express()
-const port = 8000 
+const port = process.env.PORT || 8000 
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
